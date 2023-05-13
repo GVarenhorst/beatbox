dfbf0442b1bb8bb6e24a19a55f30dd9f19f56480
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-05-13 14:38:24 -0500

    Add append method to linked_list.rb

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -3,4 +3,30 @@ class LinkedList
   def initialize
     @head = nil
   end
+
+  def append(data)
+    if @head == nil
+      @head = Node.new(data)
+    else
+      current_node = @head
+    while current_node.next_node != nil
+        current_node = current_node.next_node
+    end
+      current_node.next_node = Node.new(data)
+    end
+  end
+
+  #  def count
+  #   if self == nil
+  #     count = 0
+  #   else
+  #     count = 1
+  #     current_node = @head
+  #     while current_node.next_node != nil
+  #       current_node = current_node.next_node
+  #       count +=1
+  #     end
+  #     count
+  #   end 
+  # end
 end
