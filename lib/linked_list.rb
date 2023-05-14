bf9b031fc3edc34fcc5102067b36bed0cce6abdb
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-05-13 21:09:43 -0500

    Fix count method with rewrite of code to allow append of more than one node and recognize count

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -9,26 +9,36 @@ class LinkedList
       @head = Node.new(data)
     else
       current_node = @head
+      new_node = Node.new(data)
     while current_node.next_node != nil
         current_node = current_node.next_node
-    end
-      current_node.next_node = Node.new(data)
+      end
+      current_node.next_node = new_node
     end
   end
 
   def count
-  if @head.nil?
-    count = 0
-  else
-    count = 1
+    counter = 0
     current_node = @head
-    while current_node.next_node != nil
+    until current_node == nil
       current_node = current_node.next_node
-      count +=1
+      counter += 1
     end
-    count
-    end 
+    counter
   end
+  # def count
+  #   if @head.nil?
+  #     count = 0
+  #   else
+  #     current_node = @head
+  #     count = 1
+  #   while current_node != nil
+  #     current_node = current_node.next_node
+  #     count +=1
+  #   end
+  #   count
+  #   end
+  # end
 
   def to_string
     current_node = @head
