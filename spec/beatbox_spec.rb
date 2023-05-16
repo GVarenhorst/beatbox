c48846675cccf157cca988286cfa0379c5ae42f5
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-05-15 20:35:39 -0500

    test: Add test 'appends string data for nodes with exception for line 25 expect statement

--- spec/beatbox_spec.rb
+++ spec/beatbox_spec.rb
@@ -11,4 +11,33 @@ RSpec.describe BeatBox do
     expect(bb.list).to be_a(LinkedList)
     expect(bb.list.head).to eq(nil)
   end
+
+  it "appends string data into nodes" do
+    bb = BeatBox.new
+
+    bb.append("deep doo ditt")
+
+    expect(bb.list.head.data).to eq("deep")
+    expect(bb.list.head.next_node.data).to eq("doo")
+
+    bb.append("woo hoo shu")
+
+    # expect(bb.count).to eq(6)
+
+  end
+
+  # xit "can play sounds" do
+
+  #   bb = BeatBox.new
+
+  #   bb.append("deep doo ditt woo hoo shu")
+
+  #   expect(bb.count).to eq(6)
+  #   expect(bb.list.count).to eq(6)
+
+    # bb.play 
+
+    # expect(bb.play).to_be_truthy
+
+  # end
 end
